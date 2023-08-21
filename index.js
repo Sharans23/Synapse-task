3382801ada8a974ea01c421d0fa693f67ac6824f
Sharans23 <141628446+Sharans23@users.noreply.github.com>
2023-08-22 00:43:57 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-$(".cards").on("mouseover", function() {
-    $(".project").css("color", "#245953");
-});
-$(".cards").on("mouseover", function() {
-    $(".card-body").css("color", "#245953");
-});
-
