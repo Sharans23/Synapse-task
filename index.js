a769768abb6263837b0ecf0438ed71382bfe66a4
Sharans23 <141628446+Sharans23@users.noreply.github.com>
2023-08-22 00:45:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$(".cards").on("mouseover", function() {
+    $(".project").css("color", "#245953");
+});
+$(".cards").on("mouseover", function() {
+    $(".card-body").css("color", "#245953");
+});
+
